c07d13154016086d03d402ffcd62c5ddd66e2255
Sameer Ahmad <136889939+sameerahmad005@users.noreply.github.com>
2024-04-08 14:54:12 +0530

    Add files via upload

--- files/script.js
+++ files/script.js
@@ -1,1 +1,32 @@
 
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
+
+
+
+    document.querySelector('img').addEventListener('contextmenu', function(event) {
+        event.preventDefault();
+    });
+    
+    var images = document.querySelectorAll('.abt-res');
+    images.forEach(function(img) {
+        img.addEventListener('contextmenu', function(event) {
+            event.preventDefault();
+        });
+    });
